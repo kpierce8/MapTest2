96c154e55f15c746c58635e5187d8e8ca6222237
Ken Pierce <kpierce8@gmail.com>
2012-07-13 16:50:37 -0700

    added an additional panel for wria 15

--- RCO_HRCD2012b/MainPage.xaml.cs
+++ RCO_HRCD2012b/MainPage.xaml.cs
@@ -30,7 +30,8 @@ namespace RCO_HRCD2012b
             InitializeComponent();
 
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
-
+            waView.Visibility = Visibility.Visible;
+            wria15.Visibility = Visibility.Collapsed;
            
 
         }
@@ -44,8 +45,78 @@ namespace RCO_HRCD2012b
             wriaReader.Source = new Uri("DataSources/Geospatial/EightSOS2012_WGS84s10.shp", UriKind.Relative);
             wriaReader.DataSource = new Uri("DataSources/Geospatial/EightSOS2012_WGS84s10.dbf", UriKind.Relative);
            // myReader.ReadCompleted += new ReadShapesCompletedEventHandler(myReader_ReadCompleted);
+            wriaReader.PreviewReadCompleted += new PreviewReadShapesCompletedEventHandler(wriaReader_PreviewReadCompleted);
 
 
+            myWria15Reader.Source = new Uri("DataSources/Geospatial/WRIA15/wria15_outline_W84.shp", UriKind.Relative);
+            myWria15Reader.DataSource = new Uri("DataSources/Geospatial/WRIA15/wria15_outline_W84.dbf", UriKind.Relative);
+            myWria15Reader.ReadCompleted += new ReadShapesCompletedEventHandler(myReader_ReadCompleted);
+
+            changesReader.Source = new Uri("DataSources/Geospatial/WRIA15/wria15_09changes_W84.shp", UriKind.Relative);
+            changesReader.DataSource = new Uri("DataSources/Geospatial/WRIA15/wria15_09changes_W84.dbf", UriKind.Relative);
+            // myReader.ReadCompleted += new ReadShapesCompletedEventHandler(myReader_ReadCompleted);
+            changesReader.PreviewReadCompleted += new PreviewReadShapesCompletedEventHandler(changesReader_PreviewReadCompleted);
+
+        }
+
+        void changesReader_PreviewReadCompleted(object sender, PreviewReadShapesCompletedEventArgs eventArgs)
+        {
+            foreach (var poly in eventArgs.Items)
+            {
+                if (poly is MapShape)
+                {
+                    (poly as MapShape).MouseLeftButtonDown += new MouseButtonEventHandler(wrias_MouseLeftButtonDown);
+                    (poly as MapShape).MouseEnter +=new MouseEventHandler(changes_MouseEnter);
+                    (poly as MapShape).MouseLeave += new MouseEventHandler(changes_MouseLeave);
+                }
+            }
+        }
+
+        void changes_MouseLeave(object sender, MouseEventArgs e)
+        {
+            titleBlock.Text = "";
+            acreage.Text = "";
+        }
+
+        void changes_MouseEnter(object sender, MouseEventArgs e)
+        {
+            titleBlock.Text = (sender as MapShape).ExtendedData.GetValue("PolyID").ToString();
+            acreage.Text = (sender as MapShape).ExtendedData.GetValue("Acres").ToString();
+        }
+
+
+
+
+
+
+        void wriaReader_PreviewReadCompleted(object sender, PreviewReadShapesCompletedEventArgs eventArgs)
+        {
+            foreach (var poly in eventArgs.Items) 
+            {
+                if (poly is MapShape) 
+                {
+                    (poly as MapShape).MouseLeftButtonDown += new MouseButtonEventHandler(wrias_MouseLeftButtonDown);
+                    (poly as MapShape).MouseEnter += new MouseEventHandler(wrias_MouseEnter);
+                    (poly as MapShape).MouseLeave += new MouseEventHandler(wrias_MouseLeave);
+                }
+            }
+        }
+
+        void wrias_MouseEnter(object sender, MouseEventArgs e)
+        {
+            wriaNameBlock.Text = (sender as MapShape).ExtendedData.GetValue("WRIA_NM").ToString();
+        }
+
+        void wrias_MouseLeave(object sender, MouseEventArgs e)
+        {
+            wriaNameBlock.Text = "";
+        }
+
+
+
+        void wrias_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
 
 
         }
@@ -67,5 +138,19 @@ namespace RCO_HRCD2012b
             //}
         }
 
+        private void goWria_Click(object sender, RoutedEventArgs e)
+        {
+            waView.Visibility = Visibility.Collapsed;
+            wria15.Visibility = Visibility.Visible;
+        }
+
+        private void goWA_Click(object sender, RoutedEventArgs e)
+        {
+            waView.Visibility = Visibility.Visible;
+            wria15.Visibility = Visibility.Collapsed;
+        }
+
+
+        
     }
 }
